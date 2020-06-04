540a62e8b5b6c46f2f3cc9fdc34220782c273787
Henrik jJaven <henrik@onecloud.com>
2020-06-03 17:51:36 -0700

    WIP extend

---
+++ apps/bars/Coolio4.js
@@ -0,0 +1,14 @@
+const Coolio4  =  {
+    name2: 'CoolioFour',
+    test:() => {
+        alert(Coolio4.name2)
+    },
+
+    hi: () => {
+        alert('hi')
+    },
+    bye: () =>  {
+        alert('Hi I just extended  bye')
+    }
+} 
+export default Coolio4
---
+++ apps/bars/Coolio5.js
@@ -0,0 +1,14 @@
+class Coolio5   {
+/*
+    init()  {
+        alert(Coolio4.name)
+    }
+*/
+    hi() {
+        alert('hi')
+    }
+    bye()  {
+        alert('bye5')
+    }
+} 
+export default Coolio5
