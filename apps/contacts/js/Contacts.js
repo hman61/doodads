e0854fb90c563a4d03a3a6a8d2d333debcd42493
Henrik Javen <hjaven@gmail.com>
2021-02-20 18:16:45 -0800

    added debounce

--- apps/contacts/js/Contacts.js
+++ apps/contacts/js/Contacts.js
@@ -7,14 +7,31 @@ Doo.define(
 			this.template =	this.getAttribute('template')
 			this.defaultDataSet = 'contacts'
 			Doo.DAO.append(this.defaultDataSet, new Mock(Doo.DAO.getData(this.defaultDataSet), 1000))
+			this.filterTimeoutID = null
 			this.data = {
 				[this.defaultDataSet]: Doo.DAO.getData(this.defaultDataSet)
 			}
 		}
 		find(elem) {
-			this.filterKey = elem.value
-			this.setDataFilter(this.defaultDataSet) 	
-			this.render()
+			let filterKey = elem.value.trim().length ? elem.value.trim() : ''
+			let delay = this.data[this.defaultDataSet] && this.data[this.defaultDataSet].length > 1000 ? 700 : filterKey.length > 3 ? 1 : 500
+
+			try {
+				if (elem.inputType === 'deleteContentBackward' && this.foundRecords > 100) {
+					delay = 700
+				}
+			} catch (err) {
+				// justy in case the browser doesn't suppoer 'deleteContentBackward'
+			}
+			if (this.filterTimeoutID) {
+				clearTimeout(this.filterTimeoutID)
+			}
+			this.filterTimeoutID = setTimeout(() => {
+					this.filterKey = elem.value
+					this.setDataFilter(this.defaultDataSet) 	
+					this.render()
+		
+			}, delay)
 		}
 
 		async add(elem) {
